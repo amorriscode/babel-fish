3ace0e828dcfb6a19d3978609e444457cac530fa
Anthony Morris <amorriscode@gmail.com>
2024-04-13 14:26:39 -0700

    feat: adjust landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import '@fontsource/press-start-2p';
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	const fish = `<><`;
 	import { goto } from '$app/navigation';
 	import { PUBLIC_SOCKET_URL } from '$env/static/public';
 
@@ -11,6 +12,19 @@
 	}
 </script>
 
-<h1 class="text-3xl font-bold">babel fish</h1>
+<div class="h-screen flex items-end p-4 gap-12">
+	<div>
+		<h1 class="text-6xl mb-2 flex items-center">
+			babel_fish <span class="whitespace-nowrap tracking-[-0.5em] text-5xl">{fish}</span>
+		</h1>
+		<button
+			on:click={createRoom}
+			class="bg-rose-500 hover:bg-rose-600 text-4xl p-4 w-full text-white">create room</button
+		>
+	</div>
 
-<button on:click={createRoom}>create room</button>
+	<div class="text-right text-sm">
+		If you stick a Babel fish in your ear you can instantly understand anything said to you in any
+		form of language.
+	</div>
+</div>
